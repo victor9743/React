75721bdf2eb9260e01050266a4f72f64c75965b0
Victor Costa <70709626+victor9743@users.noreply.github.com>
2023-10-09 20:37:14 -0300

    hotfix/reactbasico
    continuacao aula 16

--- Exercicios React/react_basico/projeto_1/src/App.js
+++ Exercicios React/react_basico/projeto_1/src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>{1+1} Hello Wolrd</h1>
-    </div>
-  );
+// componente de classe
+
+class App extends Component {
+  state = {
+    name: 'Victor Costa',
+    counter: 0
+  }
+
+  handlePClick () {
+    // manipulando estados
+    this.setState({ name: "Hello Update"});
+  }
+
+  // event= manipulando eventos de onclick e outros
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+
+    this.setState({ counter: counter + 1});
+  }
+
+  render(){
+    const {name, counter} = this.state;
+    return ( 
+      <>
+        <p onClick={this.handlePClick } >{name}</p>
+        <div>
+          <h3>Contador: {counter}</h3>
+        </div>
+
+        <div>
+          <a onClick={this.handleAClick} href="https://reactjs.org">Link</a>
+        </div>
+      </>
+    )
+  }
 }
+
+// componentes de funcao
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>{1+1} Hello Wolrd</h1>
+//     </div>
+//   );
+// }
 
 export default App;
