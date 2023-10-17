6f72458bcf4b77f8d2b623b1a254b5e2d8106268
Victor Costa <70709626+victor9743@users.noreply.github.com>
2023-10-17 20:31:36 -0300

    feature
    continuacao: aula 17
    parado: aula 20

--- udemy/modulo 2/projeto1/src/App.js
+++ udemy/modulo 2/projeto1/src/App.js
@@ -1,77 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
 
-  // criando um método construtor
-  // constructor(props) {
-  //   super(props);
-    // adicionando metodos da propria classe usando boundfunction
-    // this.metodoclick = this.metodoclick.bind(this);
 
-    state = {
-      name: 'Victor Costa',
-      counter: 0
-    };
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    this.loadPosts();
+  }
+
+
+  loadPosts = async () => {
+    const postsResponse = fetch("https://jsonplaceholder.typicode.com/posts");
+    const photosResponse = fetch("https://jsonplaceholder.typicode.com/photos");
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post, index) => {
+      return {
+        ...post, cover: photosJson[index].url
+      }
+    });
+
+    this.setState({posts: postsAndPhotos});
+  }
+
+  // componentDidUpdate() {
   // }
 
-  metodoclick = () => {
-    // mudando valor de um estado de um objeto
-    this.setState({ name: 'Outro Nome' });
-  }
-
-  counterteste = (e) => {
-    let { counter } = this.state;
-
-    this.setState({ counter : counter + 1});
-    e.preventDefault();
-  }
+  // componentWillUnmount() {
+  // }
 
   render () {
 
-    const { name, counter }  = this.state;
+    const { posts } = this.state;
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p onClick={ this.metodoclick}>
-            {name} - {counter}
-          </p>
-          <a
-            onClick={ this.counterteste }
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <section className='container'>
+        <div className="posts">
+          {posts.map( post => (
+              <div className='post'>
+                <img src={post.cover} alt={post.title} />
+                <div key={post.id} className='post-content' >
+                  <h1>{post.title}</h1>
+                  <p>{post.body}</p>
+                </div>
+              </div>
+          ))}
+        </div>
+      </section>
     );
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Hello Wolrd
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
 export default App;
