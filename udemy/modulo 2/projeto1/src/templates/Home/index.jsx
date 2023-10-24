1b655df95b2167f95c6891c03005c86c3aae93c3
Victor Costa <70709626+victor9743@users.noreply.github.com>
2023-10-23 21:05:51 -0300

    feature
    continuacao: aula 24
    parado: aula 25

--- udemy/modulo 2/projeto1/src/templates/Home/index.jsx
+++ udemy/modulo 2/projeto1/src/templates/Home/index.jsx
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import {Posts} from '../../components/Posts'
 import {loadPosts} from "../../utils/load-posts"
 import { Button } from '../../components/Button';
+import { TextInput } from '../../components/TextInput';
 
 export class Home extends Component {
 
@@ -11,13 +12,13 @@ export class Home extends Component {
     posts: [],
     allPosts: [],
     page: 0,
-    postsPerPage: 10
+    postsPerPage: 10,
+    searchValue: ''
   };
 
   componentDidMount() {
     this.loadPosts();
   }
-
 
   loadPosts = async () => {
     const { page, postsPerPage} = this.state
@@ -45,21 +46,57 @@ export class Home extends Component {
     this.setState({posts, page: nextPage});
   }
 
+  handleChange = (e) => {
+    const {value} = e.target;
+
+    this.setState({searchValue: value});
+  }
+
   render () {
 
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
+    const filteredPosts =  !!searchValue ?
+    allPosts.filter(post => {
+      return post.title.toLowerCase().includes(searchValue.toLocaleLowerCase());
+    })
+    : posts;
 
+    /*
+      !! ou !
+      quando colocado em uma variavel, converte a variavel para boleano.
+      Se estiver vazia vai retornar false, se houver algum valor retorna true
+
+      &&
+      caso a condicao for verdadeira a partir deste simbolo o codigo sera executado
+    */
     return (
       <section className='container'>
-        <Posts  posts={posts}/>
-        <div className='button-container'>
-          <Button 
-            texto="load more posts" 
-            evento={this.loadMorePosts} 
-            disabled={noMorePosts}
-          />
+        <div className='search-container'>
+          {!!searchValue && (
+            <h1>Search value: {searchValue}</h1>
+          )}
+
+          <TextInput searchValue={searchValue} handleChange={this.handleChange} /> <br/><br/><br />
         </div>
+          {filteredPosts.length > 0 && (
+            <Posts  posts={filteredPosts}/>
+          )}
+
+          {filteredPosts.length === 0 && (
+            <p>Nao existem posts</p>
+          )}
+          
+
+          <div className='button-container'>
+            {!searchValue && (
+              <Button 
+              texto="load more posts" 
+              evento={this.loadMorePosts} 
+              disabled={noMorePosts}
+            />
+            )}
+          </div>
       </section>
     );
   }
