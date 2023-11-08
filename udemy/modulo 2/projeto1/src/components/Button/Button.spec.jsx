f701f342a1b1451ce28b95f63bdee0bfc8027f12
Victor Costa <70709626+victor9743@users.noreply.github.com>
2023-11-08 16:24:13 -0300

    feature
    continuação: aula 33
    parado: aula 34

---
+++ udemy/modulo 2/projeto1/src/components/Button/Button.spec.jsx
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import  userEvent from '@testing-library/user-event';
+import { Button } from '.';
+describe('<Button />', () => {
+    it('should render the button with the text "Load more"', () => {
+       render(<Button texto="Load more" />);
+       const button =  screen.getByRole('button', { name: /load more/i });
+
+        // espera que o button esteja no document
+       expect(button).toBeInTheDocument();
+
+        // espera que no button possua a classe button
+        // expect(button).toHaveAttribute('class', 'button');
+
+        // espera que no minimo haja 1 asserção
+        expect.assertions(1);
+    });
+
+
+    it('should call function on button click', () => {
+        // cria uma função mock
+        const fn = jest.fn();
+
+        // passa a função criada via parametro para a props evento.
+        render(<Button texto="Load more" evento={fn} />);
+
+        // pega o elemento button
+        const button =  screen.getByRole('button', { name: /load more/i });
+
+        // aciona o evento de click
+        // fireEvent.click(button);
+        userEvent.click(button);
+        // espera que a função seja chamada ao menos 1 vez
+        expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    it('should be disabled when disabled is true', () => {
+        render(<Button texto="Load more" disabled={true} />);
+        // pega o elemento button
+        const button =  screen.getByRole('button', { name: /load more/i });
+
+        // espera que o button esteja desativado
+        expect(button).toBeDisabled();
+    });
+    
+});
---
+++ udemy/modulo 2/projeto1/src/templates/Home/Home.test.jsx
@@ -0,0 +1,11 @@
+describe('<Home />', () => {
+  /* 
+    expect(1).toBe(1);
+    expect() - retorno
+    tobe() - valor experado
+  */
+  it('is a dummy test', () => {
+    expect(1).toBe(1);
+  });
+});
+
