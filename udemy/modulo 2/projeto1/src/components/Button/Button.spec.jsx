434282a35de0a2ea511a4f556a6c816cbfbd12c1
Victor Costa <70709626+victor9743@users.noreply.github.com>
2023-12-01 15:01:01 -0300

    feature
    recontinuação: aula 33
    parado: aula 34

--- udemy/modulo 2/projeto1/src/components/Button/Button.spec.jsx
+++ udemy/modulo 2/projeto1/src/components/Button/Button.spec.jsx
@@ -1,46 +1,77 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import  userEvent from '@testing-library/user-event';
+// import  userEvent from '@testing-library/user-event';
 import { Button } from '.';
-describe('<Button />', () => {
-    it('should render the button with the text "Load more"', () => {
-       render(<Button texto="Load more" />);
-       const button =  screen.getByRole('button', { name: /load more/i });
+// describe('<Button />', () => {
+//     it('should render the button with the text "Load more"', () => {
+//        render(<Button texto="Load more" />);
+//        const button =  screen.getByRole('button', { name: /load more/i });
 
-        // espera que o button esteja no document
-       expect(button).toBeInTheDocument();
+//         // espera que o button esteja no document
+//        expect(button).toBeInTheDocument();
 
-        // espera que no button possua a classe button
-        // expect(button).toHaveAttribute('class', 'button');
+//         // espera que no button possua a classe button
+//         // expect(button).toHaveAttribute('class', 'button');
 
-        // espera que no minimo haja 1 asserção
+//         // espera que no minimo haja 1 asserção
+//         expect.assertions(1);
+//     });
+
+
+//     it('should call function on button click', () => {
+//         // cria uma função mock
+//         const fn = jest.fn();
+
+//         // passa a função criada via parametro para a props evento.
+//         render(<Button texto="Load more" evento={fn} />);
+
+//         // pega o elemento button
+//         const button =  screen.getByRole('button', { name: /load more/i });
+
+//         // aciona o evento de click
+//         // fireEvent.click(button);
+//         userEvent.click(button);
+//         // espera que a função seja chamada ao menos 1 vez
+//         expect(fn).toHaveBeenCalledTimes(1);
+//     });
+
+//     it('should be disabled when disabled is true', () => {
+//         render(<Button texto="Load more" disabled={true} />);
+//         // pega o elemento button
+//         const button =  screen.getByRole('button', { name: /load more/i });
+
+//         // espera que o button esteja desativado
+//         expect(button).toBeDisabled();
+//     });
+    
+// });
+
+describe("<Button />" , () => {
+    it('should render the button with text', () => {
+        render(<Button texto="Load more" />);
         expect.assertions(1);
+
+        const button = screen.getByRole("button", {name: /load more/i});
+        expect(button).toBeInTheDocument();
+        // expect(button).toHaveAttribute('class', 'button');
     });
 
-
-    it('should call function on button click', () => {
-        // cria uma função mock
+    it("should call function on button click", () => {
+        // cria uma função ficticia mock
         const fn = jest.fn();
-
-        // passa a função criada via parametro para a props evento.
         render(<Button texto="Load more" evento={fn} />);
 
-        // pega o elemento button
-        const button =  screen.getByRole('button', { name: /load more/i });
+        const button = screen.getByRole("button", {name: /load more/i});
+        fireEvent.click(button);
 
-        // aciona o evento de click
-        // fireEvent.click(button);
-        userEvent.click(button);
-        // espera que a função seja chamada ao menos 1 vez
         expect(fn).toHaveBeenCalledTimes(1);
     });
 
-    it('should be disabled when disabled is true', () => {
+    it("should be disabled when disabled is true", () => {
         render(<Button texto="Load more" disabled={true} />);
-        // pega o elemento button
-        const button =  screen.getByRole('button', { name: /load more/i });
 
-        // espera que o button esteja desativado
+        const button = screen.getByRole("button", {name: /load more/i});
+        fireEvent.click(button);
+
         expect(button).toBeDisabled();
     });
-    
 });
--- udemy/modulo 2/projeto1/src/templates/Home/Home.test.jsx
+++ udemy/modulo 2/projeto1/src/templates/Home/Home.test.jsx
@@ -1,11 +1,5 @@
-describe('<Home />', () => {
-  /* 
-    expect(1).toBe(1);
-    expect() - retorno
-    tobe() - valor experado
-  */
-  it('is a dummy test', () => {
+describe("<Home />", () => {
+  it("it is a dummy test", () => {
     expect(1).toBe(1);
   });
 });
-
