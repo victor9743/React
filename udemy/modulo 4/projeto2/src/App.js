f26170755c75420a82a94edc17695ba9c132737d
Victor Costa <70709626+victor9743@users.noreply.github.com>
2024-01-26 16:38:50 -0300

    feature
    continuação: aula 48
    parado: aula 50

--- udemy/modulo 4/projeto2/src/App.js
+++ udemy/modulo 4/projeto2/src/App.js
@@ -1,35 +1,84 @@
+import P from 'prop-types';
 // import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 // import { Component } from 'react';
 
+// const eventFn = () => {
+//   console.log("h1 clicado");
+// }
+
+// React.memo: memoriza o componente caso ele não mudar. Possui algo semelhante ao useCallback, porém ele salva valores
+const Button = ({ incrementButton }) => {
+  console.log("Filho, renderizou");
+  return <button onClick={() => incrementButton(10) } >+</button>
+};
+
+// adicionando prototypes ao button
+// P.func, significa que o incrementButton será uma funcionalidade
+Button.propTypes = {
+  incrementButton: P.func,
+}
+
 // component stateless
+// useCallBack: é utilizado para renderizar funções pesadas. retorna uma função salva
 function App () {
-  const [count, setCount] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-  // componentDidUpdate - executa toda vez que o component atualiza
-  useEffect(()=> {
-    console.log("hello wolrd");
-  });
+  const incrementCounter = useCallback(
+    (number) => {
+      // setCounter(counter + number);
+      // toda vez que a alguma funcionalidade dentro do app é executada ex: clique no botão, todas as funcionalidade do método app são recarregadas
+      //  impede que a funcionalidade seja recriada cada vez que a função app for recarregada
+      setCounter((c) => c + number);
+    },[]
+  );
 
-  // componentDidMount - Executa apenas 1 vez
-  useEffect(()=> {
-    console.log("Hello wolrd apenas 1x");
-  }, []);
 
-  // com dependencia - executa toda vez que a dependencia mudar
-  // soh ira mudar caso uma determinada depndencia chamada dentro de useeffect mudar
-  useEffect(() => {
-    console.log("Contador Mudou para", count);
-  }, [count]);
+  console.log("Pai, renderizou");
 
   return (
     <div className='App'>
-      <h1>Contador: {count}</h1>
-
-      <button type='button' onClick={ () => { setCount(count + 1) }} >Incrementar</button>
+      <p>Teste 3</p>
+      <h1>C1: {counter}</h1>
+      <Button incrementButton={incrementCounter} />
     </div>
-  );
+  )
+
+  // const [count, setCount] = useState(0);
+
+  // // componentDidUpdate - executa toda vez que o component atualiza
+  // useEffect(()=> {
+  //   console.log("hello wolrd");
+  // });
+
+  // // componentDidMount - Executa apenas 1 vez
+  // useEffect(()=> {
+  //   console.log("Hello wolrd apenas 1x");
+  // }, []);
+
+  // useEffect(()=> {
+  //   document.querySelector('h1')?.addEventListener('click', eventFn);
+
+  //   // componentWillUmount - limpeza
+  //   return () => {
+  //     document.querySelector('h1')?.removeEventListener('click', eventFn);
+  //   }
+  // }, []);
+
+  // // com dependencia - executa toda vez que a dependencia mudar
+  // // soh ira mudar caso uma determinada depndencia chamada dentro de useeffect mudar
+  // useEffect(() => {
+  //   console.log("Contador Mudou para", count);
+  // }, [count]);
+
+  // return (
+  //   <div className='App'>
+  //     <h1>Contador: {count}</h1>
+
+  //     <button type='button' onClick={ () => { setCount(count + 1) }} >Incrementar</button>
+  //   </div>
+  // );
 }
 
 
